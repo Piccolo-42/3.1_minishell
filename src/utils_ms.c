27f06871ad296ad3e3beb72a1633dbe9e1b6ef6f
theblacksnow95 <enrique.murillo24@gmail.com>
2025-04-26 17:42:47 +0200

    copy_env changes error malloc

--- src/utils_ms.c
+++ src/utils_ms.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils_ms.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sravizza <sravizza@student.42lausanne.c    +#+  +:+       +#+        */
+/*   By: emurillo <emurillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 15:31:32 by sravizza          #+#    #+#             */
-/*   Updated: 2025/04/01 16:56:55 by sravizza         ###   ########.fr       */
+/*   Updated: 2025/04/26 17:42:16 by emurillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,26 @@ char	**copy_envp(char **envp)
 {
 	char	**dest;
 	int		i;
+	int		j;
 
 	i = ft_strlen_double(envp);
-	dest = malloc(sizeof(char *) * (i));
+	dest = malloc(sizeof(char *) * (i + 1));
 	if (!dest)
 		return (NULL);
 	dest[i] = NULL;
 	i = 0;
+	j = 0;
 	while (envp[i])
 	{
-		if (ft_strncmp(envp[i], "_=", 2))
-			dest[i] = ft_strdup(envp[i]);
+		if (ft_strncmp(envp[i], "_=", 2) != 0)
+		{
+			dest[j] = ft_strdup(envp[i]);
+			if (!dest[j])
+				return (NULL);
+			j++;
+		}
 		i++;
 	}
+	dest[j] = NULL;
 	return (dest);
 }
