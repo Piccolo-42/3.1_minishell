ee9365158383a78bb494f3705d1d72b03bfc1adb
theblacksnow95 <enrique.murillo24@gmail.com>
2025-04-30 11:19:41 +0200

    last pull for make

--- src/execution/execution_main.c
+++ src/execution/execution_main.c
@@ -6,7 +6,7 @@
 /*   By: emurillo <emurillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 18:05:30 by emurillo          #+#    #+#             */
-/*   Updated: 2025/04/26 18:54:00 by emurillo         ###   ########.fr       */
+/*   Updated: 2025/04/27 15:09:00 by emurillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,35 @@
 
 // gcc -Wall -Wextra -Werror -Iinclude -Ilibft  src/execution/*.c src/parsing/*.c src/builtins/ft*.c src/builtins/utils_bi.c src/utils_ms.c src/readline/*.c -lreadline -Llibft -lft -o test_execution
 
+
 int main(int argc, char **argv, char **envp)
+{
+	t_data	data;
+	char	*line;
+
+	(void)argc;
+	(void)argv;
+	data.envp = copy_envp(envp);
+	data.error = 0;
+	if (!isatty(STDIN_FILENO))
+	{
+		line = get_next_line(STDIN_FILENO);
+		while (line)
+		{
+			data.input = line;
+			data.ast = parsing(data.input);
+			if (data.ast)
+				exec_pipeline(data.ast, &data);
+			free_lst(&data.ast);
+			free(data.input);
+			line = get_next_line(STDIN_FILENO);
+		}
+	}
+	return (0);
+}
+
+
+/* int main(int argc, char **argv, char **envp)
 {
 	t_data	data;
 
@@ -38,4 +66,4 @@ int main(int argc, char **argv, char **envp)
 		free(data.input);
 	}
 	return (0);
-}
+} */
